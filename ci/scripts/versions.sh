0254d7635441f226e892aa9ace86f9dffd573d4d
Ben Sykes <hootieben@gmail.com>
2020-08-24 16:11:58 -0500

    Fix tf/tg build vers

--- ci/scripts/versions.sh
+++ ci/scripts/versions.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-jq -n --arg tfver `cat tfenv-release/tag` '{tfver: $tfver}'
+jq -n --arg tfver `cat tfenv-release/tag` --arg tgver `cat tgenv-release/tag` '{tfver: $tfver}' '{tgver: $tgver}' > relvers/build.json
